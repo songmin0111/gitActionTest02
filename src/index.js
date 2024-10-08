2ffd4eb6c04f606f7d261b062de98bd424ee6131
LSM <violet2111@naver.com>
2024-10-08 11:37:20 +0900

    first commit

---
+++ src/index.js
@@ -0,0 +1,16 @@
+// computes a factorial of a given positive integer
+function factorial(n) { // 5! = 5 * 4 * 3 * 2 * 1 =120
+
+    if( n < 0 ) {
+        throw new Error('n must be a positive integer');
+    }
+
+
+    if( n === 0 ) {
+        return 1;
+    }
+
+    return n * factorial(n - 1);
+}
+
+module.exports = factorial;
